b39bcf81038151b195d109f3a9ad4270cad47489
vedant-valid <vedant.madne66@gmail.com>
2025-08-12 23:06:27 +0530

    components and libraries added

---
+++ tailwind.config.ts
@@ -0,0 +1,14 @@
+// tailwind.config.ts
+import type { Config } from "tailwindcss";
+
+export default {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/ui/**/*.{js,ts,jsx,tsx,mdx}"
+  ],
+  theme: {
+    extend: {},
+  },
+} satisfies Config;
