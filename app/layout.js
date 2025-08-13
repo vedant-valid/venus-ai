4f5d27ddf02ce98428cbaf7f941056d1f06d6159
vedant-valid <vedant.madne66@gmail.com>
2025-08-13 23:46:07 +0530

    layout changes

--- app/layout.js
+++ app/layout.js
@@ -20,8 +20,6 @@ export default function RootLayout({ children }) {
         <body className={`${inter.className}`}>
           <Header />
           <main className="min-h-screen">{children}</main>
-          <Toaster richColors />
-
           <footer className="bg-blue-50 py-12">
             <div className="container mx-auto px-4 text-center text-gray-600">
               <p>Made with 💗 by RoadsideCoder</p>
