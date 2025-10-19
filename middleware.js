63c2b334db8b86fbeedd6ad487617c6f3ae5b1a4
vedant-valid <vedant.madne66@gmail.com>
2025-10-19 14:33:59 +0530

    authentication bug solved

--- middleware.js
+++ middleware.js
@@ -6,18 +6,13 @@ const isProtectedRoute = createRouteMatcher([
   "/transaction(.*)",
 ]);
 
-export default clerkMiddleware(async (auth, req) => {
-  const { userId } = await auth();
-
+export default clerkMiddleware(async (auth , req) => {
+  const { userId, redirectToSignIn } = await auth();
   if (!userId && isProtectedRoute(req)) {
-    const { redirectToSignIn } = await auth();
     return redirectToSignIn();
   }
 });
 
 export const config = {
-  matcher: [
-    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    '/(api|trpc)(.*)',
-  ],
+  matcher: ['/((?!_next/static|_next/image|favicon.ico).*)'],
 };
